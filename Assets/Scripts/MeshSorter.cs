f122a9d3b12613dd4e9b5c722c50fae8a9496ffd
Brian MacIntosh <brian@brianmacintosh.com>
2017-07-28 23:25:36 -0700

    lights

---
+++ Assets/Scripts/MeshSorter.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class MeshSorter : MonoBehaviour
+{
+	private MeshRenderer m_renderer;
+
+	public int OrderInLayer
+	{
+		get { return m_renderer.sortingOrder; }
+		set
+		{
+			m_orderInLayer = value;
+			m_renderer.sortingOrder = value;
+		}
+	}
+	[SerializeField]
+	private int m_orderInLayer = 0;
+
+	public string SortingLayer
+	{
+		get { return m_sortingLayer; }
+		set
+		{
+			m_sortingLayer = value;
+			m_renderer.sortingLayerName = value;
+		}
+	}
+	[SerializeField]
+	private string m_sortingLayer;
+
+	private void OnValidate()
+	{
+		if (m_renderer == null)
+		{
+			m_renderer = GetComponent<MeshRenderer>();
+		}
+		m_renderer.sortingOrder = m_orderInLayer;
+		m_renderer.sortingLayerName = m_sortingLayer;
+	}
+}
---
+++ Assets/Scripts/ScaleToCameraPlane.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Scales the object to fill the entire plane of the main camera at its depth.
+/// </summary>
+[ExecuteInEditMode]
+public class ScaleToCameraPlane : MonoBehaviour
+{
+	[SerializeField]
+	private Camera m_camera = null;
+
+	private int m_lastScreenWidth = 0;
+	private int m_lastScreenHeight = 0;
+
+	private Vector3[] m_corners = new Vector3[4];
+
+	public float ManualScale = 0.1f;
+
+	public bool DisableX;
+	public bool DisableY;
+
+	private void OnValidate()
+	{
+		UpdateScale();
+	}
+
+	private void Update()
+	{
+		if (m_lastScreenWidth != m_camera.pixelWidth
+			|| m_lastScreenHeight != m_camera.pixelHeight)
+		{
+			m_lastScreenWidth = m_camera.pixelWidth;
+			m_lastScreenHeight = m_camera.pixelHeight;
+			UpdateScale();
+		}
+	}
+
+	private void UpdateScale()
+	{
+		if (m_camera.orthographic)
+		{
+			transform.localScale = new Vector3(
+				m_camera.orthographicSize * m_camera.pixelWidth / m_camera.pixelHeight,
+				transform.localScale.y,
+				m_camera.orthographicSize) * ManualScale;
+		}
+		else
+		{
+			m_camera.CalculateFrustumCorners(
+				new Rect(0f, 0f, 1f, 1f),
+				m_camera.transform.position.z - transform.position.z,
+				Camera.MonoOrStereoscopicEye.Mono,
+				m_corners);
+			transform.localScale = new Vector3(
+				DisableX ? transform.localScale.x : m_corners[1].x - m_corners[2].x,
+				transform.localScale.y,
+				DisableY ? transform.localScale.z : m_corners[0].y - m_corners[1].y) * ManualScale;
+		}
+	}
+}
