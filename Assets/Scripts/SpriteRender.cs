f8961d5eb7429cead99075d05a9e43e3886c78a5
Brian MacIntosh <brian@brianmacintosh.com>
2017-07-29 01:00:42 -0700

    using unity lights

---
+++ Assets/Scripts/SpriteRender.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRender : MonoBehaviour
+{
+	private Renderer m_renderer;
+
+	[SerializeField]
+	private bool m_receiveShadows = false;
+
+	private void OnValidate()
+	{
+		if (m_renderer == null)
+		{
+			m_renderer = GetComponent<Renderer>();
+		}
+		m_renderer.receiveShadows = m_receiveShadows;
+	}
+}
