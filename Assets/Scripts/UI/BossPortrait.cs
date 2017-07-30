643ecdb40a3cd0f5c1273cfe78eb8c8285411159
Brian MacIntosh <brian@brianmacintosh.com>
2017-07-30 07:46:05 -0700

    made UI fit

---
+++ Assets/Scripts/UI/BossPortrait.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossPortrait : MonoBehaviour
+{
+	[SerializeField]
+	private Image m_portraitImage = null;
+}
