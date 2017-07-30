f6437fe1bf8a946eaebdfd56e34b8e4b7e5a5aa9
Brian MacIntosh <brian@brianmacintosh.com>
2017-07-30 00:02:05 -0700

    ghost eyes. hi alexis.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance
+	{
+		get
+		{
+			if (s_instance == null)
+			{
+				s_instance = FindObjectOfType<GameManager>();
+			}
+			return s_instance;
+		}
+	}
+	private static GameManager s_instance;
+
+	public GameObject[] Players
+	{
+		get
+		{
+			TryFindPlayers();
+			return m_players;
+		}
+	}
+	private GameObject[] m_players;
+
+	public GameObject Beep { get; private set; }
+
+	public GameObject Boop { get; private set; }
+
+	private void TryFindPlayers()
+	{
+		if (m_players == null)
+		{
+			m_players = GameObject.FindGameObjectsWithTag("Player");
+			foreach (GameObject obj in m_players)
+			{
+				if (obj.GetComponent<Player_Navigation>().isBeep)
+				{
+					Beep = obj;
+				}
+				else
+				{
+					Boop = obj;
+				}
+			}
+		}
+	}
+}
--- Assets/Scripts/UI/EnergyUI.cs
+++ Assets/Scripts/UI/EnergyUI.cs
@@ -1,34 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EnergyUI : MonoBehaviour {
-    public bool isBeep = true;
-    public GameObject player;
-	// Use this for initialization
-	void Start ()
-    {
-        bool beepIs1 = false;
-        GameObject[] playerList = GameObject.FindGameObjectsWithTag("Player");
-        if((playerList[0]).GetComponent<Player_Navigation>().isBeep)
-        {
-            beepIs1 = true;
-        }
-        if((isBeep && beepIs1) || (!isBeep && !beepIs1))
-        {
-            player = playerList[0];
-        }
-        else
-        {
-            player = playerList[1];
-        }
+public class EnergyUI : MonoBehaviour
+{
+	public bool isBeep = true;
+	public GameObject player;
 
+	void Start()
+	{
+		if (isBeep)
+		{
+			player = GameManager.Instance.Beep;
+		}
+		else
+		{
+			player = GameManager.Instance.Boop;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        GetComponent<Image>().fillAmount = player.GetComponent<PlayerEnergy>().currentEnergy / 100f;
+
+	void Update()
+	{
+		GetComponent<Image>().fillAmount = player.GetComponent<PlayerEnergy>().currentEnergy / 100f;
 	}
 }
