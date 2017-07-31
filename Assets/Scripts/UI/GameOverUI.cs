6ba1094658fe0ad2cebfebc47e0ea48e1c8fe7b2
Brian MacIntosh <brian@brianmacintosh.com>
2017-07-30 21:42:10 -0700

    reset, game over

---
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverUI : MonoBehaviour
+{
+	public GameObject Content;
+
+	private void Start()
+	{
+		Content.SetActive(false);
+	}
+
+	private void Update()
+	{
+		if (Content.activeSelf)
+		{
+			if (Input.GetButton("Start"))
+			{
+				SceneLoader.Instance.ReloadCurrentScene();
+			}
+		}
+		else
+		{
+			bool anyHasEnergy = false;
+			foreach (GameObject player in GameManager.Instance.Players)
+			{
+				if (player.GetComponent<PlayerEnergy>().HasEnergy)
+				{
+					anyHasEnergy = true;
+					break;
+				}
+			}
+			if (!anyHasEnergy)
+			{
+				Content.SetActive(true);
+			}
+		}
+	}
+}
