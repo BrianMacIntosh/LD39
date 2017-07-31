8326ae151549ca72dddc0f2271a198db4cf3e913
Brian MacIntosh <brian@brianmacintosh.com>
2017-07-30 18:01:20 -0700

    fixed render texture size problems

---
+++ Assets/Scripts/RenderManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderManager : MonoBehaviour
+{
+	private RenderTexture m_lightRenderTexture = null;
+
+	[SerializeField]
+	private Material m_lightPassMaterial = null;
+
+	private Camera m_camera;
+
+	private void Awake()
+	{
+		m_camera = GetComponent<Camera>();
+		CreateRenderTexture();
+	}
+
+	private void OnPreRender()
+	{
+		if (m_lightRenderTexture.width != Screen.width
+			|| m_lightRenderTexture.height != Screen.height)
+		{
+			CreateRenderTexture();
+		}
+	}
+
+	private void CreateRenderTexture()
+	{
+		if (m_lightRenderTexture)
+		{
+			Destroy(m_lightRenderTexture);
+		}
+		m_lightRenderTexture = new RenderTexture(Screen.width, Screen.height, 0);
+		m_lightPassMaterial.mainTexture = m_lightRenderTexture;
+		m_camera.targetTexture = m_lightRenderTexture;
+	}
+}
