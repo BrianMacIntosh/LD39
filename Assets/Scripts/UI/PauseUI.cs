f21cec88bb5968206c11d831a224f9d641a9f9c0
Brian MacIntosh <brian@brianmacintosh.com>
2017-07-30 18:11:07 -0700

    pause UI

---
+++ Assets/Scripts/UI/PauseUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseUI : MonoBehaviour
+{
+	public GameObject Contents;
+
+	private void Start()
+	{
+		Contents.SetActive(false);
+	}
+
+	void Update()
+	{
+		bool pauseControl = Input.GetButtonDown("Pause");
+		bool startControl = Input.GetButtonDown("Start");
+		if (Time.timeScale > 0f && pauseControl)
+		{
+			Contents.SetActive(true);
+			Time.timeScale = 0f;
+		}
+		else if (Time.timeScale < 1f && (pauseControl || startControl))
+		{
+			Contents.SetActive(false);
+			Time.timeScale = 1f;
+		}
+	}
+}
