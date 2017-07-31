fb68a9abce7c0480a492ab014750e77aa283a293
Brian MacIntosh <brian@brianmacintosh.com>
2017-07-30 23:15:03 -0700

    fixed inappropriate powerade

---
+++ Assets/Scripts/UI/GetCurrentSceneObjectiveSprite.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetCurrentSceneObjectiveSprite : MonoBehaviour
+{
+	private Image m_image = null;
+
+	void Start()
+	{
+		m_image = GetComponent<Image>();
+		
+		if (SceneLoader.Instance)
+		{
+			SceneLoader.Instance.OnSceneChanged += OnSceneChanged;
+		}
+		OnSceneChanged(FindObjectOfType<SceneParent>());
+	}
+
+	void OnSceneChanged(SceneParent newScene)
+	{
+		m_image.sprite = newScene.ObjectiveSprite;
+	}
+}
